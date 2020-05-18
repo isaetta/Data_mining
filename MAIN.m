3fbb50b1198550d23b5516888d9a88227c739c9f
Chiara Nardin <chiara.nardin@unitn.it>
2020-05-18 11:10:31 +0200

    Aggiunta logica lavoro

--- MAIN.m
+++ MAIN.m
@@ -1,8 +1,24 @@
 %% MAIN - DATA MINING
 % Code for identifyng possible correlations inside acquired data
 
+%% RUN of DATA
+% a) SEISMIC
+% b) RANDOM
 
 %% FUNCTIONS
-% i) Spostamenti sommità serbatoio:
-%      - verticali ;  - orizzontali
- 
+% Output = array [Nx1] that contains max value for each N-run of test 
+% 
+% i)    Spostamenti sommità serbatoio:
+%       - verticali ;  - orizzontali
+% ii)   Spostamenti ID sommità serbatoio - base serbatoio:
+%       - verticali;   - orizzontali
+% iii)  Taglio al piano
+% iv)   Taglio base serbatoio (ridistribuzione forza taglio)
+% v)    Spettro accelerazione al piano
+% vi)   Spettro spostamento al piano
+% vii)  PGA segnale
+% viii) Sa(T1) - Sa(T2) - Sa(T3)
+% ix)   Sd(T1) - Sd(T2) - Sd(T3)
+
+%% SCATTER of DATA COLLECT
+
